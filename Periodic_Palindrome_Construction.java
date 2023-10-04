45c0036d32f0913fd348ef8c4e46f7562cc4d305
Sagar Gupta <sagarguptha35@gmail.com>
2023-10-04 15:47:12 +0530

    Periodic palindrom problem

--- Periodic_Palindrome_Construction.java
+++ Periodic_Palindrome_Construction.java
@@ -1,13 +1,11 @@
-package CodeChef;
-
+/* package codechef; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-import java.util.function.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class The_Attack_of_Knight
+class Periodic_Palindrome_Construction
 {
     static class FastReader {
         BufferedReader br;
@@ -70,7 +68,6 @@ class The_Attack_of_Knight
             bw.close();
         }
     }
-    
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
@@ -78,57 +75,15 @@ class The_Attack_of_Knight
         FastWriter out = new FastWriter();
         int t = in.nextInt();
         while(t-- > 0){
-            int x1 = in.nextInt();
-            int y1 = in.nextInt();
-            int x2 = in.nextInt();
-            int y2 = in.nextInt();
-            if(method(x1, x2, 1, 1)){
-                if(method(y1, y2, 2, 2)) {
-//                    System.out.println("YES");
-                    out.println("YES");
-                    continue;
-                }
-            } if(method(x1, x2, 1, 2)){
-                if(method(y1, y2, 2, 1)) {
-//                    System.out.println("YES");
-                    out.println("YES");
-                    continue;
-                }
-            } if(method(x1, x2, 2, 1)){
-                if(method(y1, y2, 1, 2)) {
-//                    System.out.println("YES");
-                    out.println("YES");
-                    continue;
-                }
-            } if(method(x1, x2, 2, 2)){
-                if(method(y1, y2, 1, 1)) {
-//                    System.out.println("YES");
-                    out.println("YES");
-                    continue;
-                }
+            int n = in.nextInt();
+            int p = in.nextInt();
+            if(p <= 2) {
+                System.out.println("impossible");
+                continue;
             }
-
-//            System.out.println("NO");
-            out.println("NO");
+            String period = "a"+"b".repeat(p-2)+"a";
+            System.out.println(period.repeat(n/p));
         }
-        
         out.close();
 	}
-	
-	public static boolean method(int a, int b, int c, int d){
-        Predicate<Integer> valid = i -> (i<=8 && i>= 1);
-        int k1 = a-c;
-        int k2 = a+c;
-        int k3 = b-d;
-        int k4 = b+d;
-        if(valid.test(k1)){
-            if(valid.test(k3)) if(k1 == k3) return true;
-            if(valid.test(k4)) if(k1 == k4) return true;
-        }
-        if(valid.test(k2)){
-            if(valid.test(k3)) if(k2 == k3) return true;
-            if(valid.test(k4)) return k2 == k4;
-        }
-        return false;
-    }
 }
