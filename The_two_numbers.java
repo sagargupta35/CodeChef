0f0fc38ceb2450d39d004cee9203dfe62eae0a6e
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2023-10-08 22:14:58 +0530

    Create The_two_numbers.java
    problem 1 -08/10/2023:
    lcm * gcd =a*b
    (n-k)*(k) is maximum at k = n/2

---
+++ The_two_numbers.java
@@ -0,0 +1,109 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class The_two_numbers
+{
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0){
+            int n = in.nextInt();
+            if(n == 2){
+                System.out.println(0);
+                continue;
+            }
+            if((n & 1) == 1){
+                long p1 = (long) n/2;
+                for(int i = 0,j = 1; p1-i>0; i++, j++){
+                    if(getGcd(p1-i, p1+j) == 1){
+                        System.out.println((p1-i)*(p1+j) - 1);
+                        break;
+                    }
+                }
+                continue;
+            }
+            long p1 = (long) n/2;
+            for(int i = 1; p1-i>0; i++){
+                if(getGcd(p1-i, p1+i) == 1){
+                    System.out.println((p1-i)*(p1+i) - 1);
+                    break;
+                }
+            }
+        }
+        out.close();
+	}
+	
+	public static long getGcd(long a, long b){
+        if(b == 0) return a;
+        if(a == 0) return b;
+        return getGcd(b, a%b);
+    }
+}
