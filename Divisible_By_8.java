50398ad5239379f4ea6e7e442735f0c2b52f1d4e
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2023-10-11 22:37:02 +0530

    Create Divisible_By_8.java
    problem 3 - Startes 103 - reached 3 starts (1601)!! - 11/10/2023

---
+++ Divisible_By_8.java
@@ -0,0 +1,99 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		        FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0){
+            int n = in.nextInt();
+            StringBuilder str = new StringBuilder(in.next());
+            if(str.length() == 1){
+                out.println(8);
+                continue;
+            }
+            int k =  Integer.parseInt(""+ str.charAt(str.length()-1));
+            BigInteger s = new BigInteger(str.toString());
+            int r1 = s.mod(new BigInteger("8")).intValue();
+            int r2 = 8 - r1;
+            if(r2!=8) {
+                int f = 0;
+                if(k - r1 < 0) f = k+r2;
+                else f = k-r1;
+                str.deleteCharAt(str.length() - 1).append(f);
+                out.println(str);
+            } else out.println(str);
+        }
+        out.close();
+	}
+}
