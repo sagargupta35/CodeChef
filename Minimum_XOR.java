56c3b58774ad168a01d8784118b3169dc4685967
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2023-11-22 22:19:49 +0530

    Create Minimum_XOR.java
    problem 4 - 22/11/2023 - starters 109 - array and xor

---
+++ Minimum_XOR.java
@@ -0,0 +1,94 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+        static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader in = new FastReader();
+        FastWriter out =  new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0){
+            int n = in.nextInt();
+            int [] arr = new int [n];
+            long res = 0;
+            long res2 = Integer.MAX_VALUE;
+            for(int i = 0;i<n; i++){
+                arr[i] = in.nextInt();
+                res ^= arr[i];
+            }
+            res2 = res;
+            for(int i = 0; i<n; i++){
+                res2 = Math.min(res2, res^arr[i]);
+            }
+            out.println(res2);
+        }
+        out.close();
+	}
+}
