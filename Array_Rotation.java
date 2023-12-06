7e91b2a46422f5dc0c6416028c0f28c66174e2ac
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2023-12-06 16:59:09 +0530

    Create Array_Rotation.java
    problem 2 - 06/12/2023 - arrays and observation and modulus

---
+++ Array_Rotation.java
@@ -0,0 +1,91 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int n = in.nextInt();
+        long sum = 0;
+        long mod = (long) (Math.pow(10, 9) + 7);
+        for(int i = 0; i<n; i++){
+            sum += in.nextInt();
+            sum=(sum+mod)%mod;
+        }
+        int q = in.nextInt();
+        while(q-- > 0){
+            int k = in.nextInt();
+            sum=(sum*2)%mod;
+            out.println(sum);
+        }
+        out.close();
+	}
+}
