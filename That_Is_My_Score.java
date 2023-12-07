a6f2c686c2833464df6877bcf74fb3156bd87672
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2023-12-07 14:28:26 +0530

    Create That_Is_My_Score.java
    problem 1 - 07/12/2023 - 100TH DAY STREAK - math and array

---
+++ That_Is_My_Score.java
@@ -0,0 +1,94 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+        static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0){
+            int n = in.nextInt();
+            int [] arr = new int [8];
+            while(n-- > 0){
+                int i = in.nextInt();
+                int s = in.nextInt();
+                if(i < 9) arr[i-1] = Math.max(arr[i-1], s);
+            }
+            long sum = 0;
+            for(int i = 0; i<8; i++){
+                sum += arr[i];
+            }
+            out.println(sum);
+        }
+        out.close();
+	}
+}
