66a7d3827690dea5d6c5590bde43e6aba6122ef2
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2023-12-31 14:59:21 +0530

    Create MEX_OR.java
    problem 2 - 31-12-2023 - bitwise OR and observation

---
+++ MEX_OR.java
@@ -0,0 +1,97 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+        static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while (t-- > 0){
+            int x = in.nextInt();
+            if(x == 0){
+                out.println(1);
+                continue;
+            }
+            int pow = (int) Math.floor(Math.log(x)/Math.log(2));
+            if(((x+1) & x) == 0){
+                int j = 1;
+                j <<= (pow+1);
+                out.println(j);
+            } else{
+                int j = 1;
+                j <<= pow;
+                out.println(j);
+            }
+        }
+        out.close();
+	}
+}
