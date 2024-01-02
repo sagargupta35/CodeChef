1f4501ec49d69a806bd15afa135e271dbb3e2338
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-01-02 13:32:06 +0530

    Create Am_I_a_Fibonacci_Number.java
    problem 2 - 02-01-2024 - fibonacci number

---
+++ Am_I_a_Fibonacci_Number.java
@@ -0,0 +1,93 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+        static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        FastReader in = new FastReader();
+        FastWriter out =  new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0){
+            String s = in.nextLine();
+            BigInteger b = new BigInteger(s);
+            BigInteger multiply = b.pow(2).multiply(BigInteger.valueOf(5L));
+            BigInteger f = multiply.subtract(BigInteger.valueOf(4L));
+            BigInteger sec = multiply.add(BigInteger.valueOf(4L));
+            BigInteger fs = f.sqrt();
+            BigInteger ss = sec.sqrt();
+            if(fs.pow(2).equals(f) || ss.pow(2).equals(sec)){
+                out.println("YES");
+            } else out.println("NO");
+        }
+        out.close();
+	}
+}
