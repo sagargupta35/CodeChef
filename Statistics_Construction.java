f416787507988208087b80cd57ea5a690aa8848c
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-01-04 15:08:45 +0530

    Create Statistics_Construction.java
    problem 3 - 04-01-2024 - mean, median and equations

---
+++ Statistics_Construction.java
@@ -0,0 +1,91 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+        static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader in = new FastReader();
+        FastWriter out =  new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0) {
+            int n = in.nextInt();
+            int st, end = n + n / 2;
+            if (n % 2 == 0) {
+                st = n - (n / 2) + 1;
+            } else {
+                st = n - n / 2;
+            }
+            for (int i = st; i <= end; i++) out.print(i + " ");
+            out.println("");
+        }
+        out.close();
+	}
+}
