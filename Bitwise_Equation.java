28644cf5ac1453e7b53a2f7bfaf603d49d7cc781
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-01-09 10:06:30 +0530

    Create Bitwise_Equation.java
    problem 1 - 09-01-2024 - forming the bitwise equation 
    n = ((a&b)|c)^d

---
+++ Bitwise_Equation.java
@@ -0,0 +1,93 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+        static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0) {
+            long n = in.nextLong();
+            if(n == 0){
+                out.println(14 + " " + 6 + " " + 1 + " " + 7);
+                continue;
+            }
+            int p = (int) Math.ceil(Math.log(n)/Math.log(2));
+            long f = 2L << p;
+            long s = 2L << (p+1);
+            long a = f+s;
+            out.println(a + " " + (f + 1) + " " + n + " " + f);
+
+        }
+        out.close();
+	}
+}
