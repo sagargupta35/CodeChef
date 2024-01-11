7b452ec0284a8333f5279800b016f287f9819092
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-01-11 15:38:11 +0530

    Create Weight_of_Numbers.java
    problem 2  - 11-10-2024 - math and fastPower

---
+++ Weight_of_Numbers.java
@@ -0,0 +1,105 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+        static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        int mod = (int) Math.pow(10, 9) + 7;
+        while(t-- > 0) {
+            long n = in.nextLong();
+            int w = in.nextInt();
+            if(w < -9 || w > 8){
+                out.println(0);
+                continue;
+            }
+            w = w>=0 ? 9-w : 10+w;
+            long res = fastPower(10, n-2, mod);
+            res = (res*w)%mod;
+            out.println(res);
+        }
+        out.close();
+	}
+	
+	static long fastPower(long a, long b, long mod){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+}
