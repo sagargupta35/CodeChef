b3f4295d33cd12fe61f5f8c83b515e2146262559
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-02-06 16:21:28 +0530

    Create My_Fair_Coins.java
    problem 1 - 06-02-2024 - matrixc exponentiation

---
+++ My_Fair_Coins.java
@@ -0,0 +1,210 @@
+import java.util.*;
+import java.io.*;
+import java.util.function.*;
+import java.util.regex.Pattern;
+
+class CodeChef{
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    static FastReader in = new FastReader();
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static int aMax(int [] arr){
+        int a = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++) a = Math.max(a, arr[i]);
+        return a;
+    }
+
+    static int aMin(int [] arr){
+        int a = Integer.MAX_VALUE;
+        for(int i = 0; i<arr.length; i++) a = Math.min(a, arr[i]);
+        return a;
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    static int mod = (int) Math.pow(10, 9) + 7;
+
+    static long lcm(long a, long b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    static long sumOfNumbers(long start, long end, int mod){
+        if(end < start) return 0;
+        else if(end == start) return end;
+        return (((end*(end+1))/2) - ((start*(start-1))/2)) %mod;
+    }
+
+    static class Pair<T, U>{
+        T f;
+        U s;
+        public Pair(T first, U sec){
+            this.f = first;
+            this.s = sec;
+        }
+    }
+
+    public static void main (String[] args) throws java.lang.Exception {
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0) {
+            int n = in.nextInt();
+            if(n == 1){
+                System.out.println(1);
+                continue;
+            }
+            if(n == 2){
+                System.out.println(3);
+                continue;
+            }
+            long [][] base = {{2, 2}, {1, 0}};
+            long [][] res = matrixExponentiation(base, n-2);
+            System.out.println(((res[0][0]*3)%mod + (res[0][1])%mod)%mod);
+        }
+        out.close();
+    }
+    
+     static long [][] multiplyMatrix(long [][] a, long [][] b){
+        int n1 = a.length;
+        int n2 = a[0].length;
+        int n3 = b.length;
+
+        long [][] res = new long [n1][n3];
+
+        for(int i = 0; i<n1; i++){
+            for(int j = 0; j<n2; j++){
+                long val = 0;
+                for(int k = 0; k<n3; k++){
+                    val = (val + (((a[i][k]%mod)%mod)*(b[k][j]%mod))%mod)%mod;
+                }
+                res[i][j] = val;
+            }
+        }
+
+        return res;
+    }
+
+    static long [][] matrixExponentiation(long [][] mat, int n){
+        long [][] res = new long [mat.length][mat[0].length];
+        for(int i = 0; i<res.length&&i<res[0].length; i++){
+            res[i][i] = 1;
+        }
+        while (n > 0){
+            if(n%2 != 0){
+                res = multiplyMatrix(res, mat);
+            }
+            mat = multiplyMatrix(mat, mat);
+            n/=2;
+        }
+        return res;
+    }
+}
