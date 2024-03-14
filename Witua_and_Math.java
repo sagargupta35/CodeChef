8156e6729cafca2722546a9556c497ab9ab1234b
Sagar Gupta <144796569+sagargupta35@users.noreply.github.com>
2024-03-14 22:24:59 +0530

    Create Witua_and_Math.java
    problem 2 - 14-03-2024 - miller robin primality check

---
+++ Witua_and_Math.java
@@ -0,0 +1,320 @@
+import java.util.*;
+import java.io.*;
+import java.util.function.*;
+import java.util.regex.Pattern;
+import java.math.*;
+
+class CodeChef{
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("").append(String.valueOf(object));
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+    static FastReader in = new FastReader();
+    static int [] ai(int n){
+        int [] arr = new int [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextInt();
+        return arr;
+    }
+
+    static long [] al(int n){
+        long [] arr = new long [n];
+        for(int i = 0; i<n; i++) arr[i] = in.nextLong();
+        return arr;
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static int aMax(int [] arr){
+        int a = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++) a = Math.max(a, arr[i]);
+        return a;
+    }
+
+    static int aMin(int [] arr){
+        int a = Integer.MAX_VALUE;
+        for(int i = 0; i<arr.length; i++) a = Math.min(a, arr[i]);
+        return a;
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    static int mod = (int) Math.pow(10, 9) + 7;
+
+    static long lcm(long u, long v) {
+        return (u / gcd(u, v)) * v;
+    }
+
+    static long sumOfNumbers(long start, long end){
+        if(end < start) return 0;
+        else if(end == start) return end;
+        return (((end*(end+1))/2) - ((start*(start-1))/2));
+    }
+
+    static class Pair<T, U>{
+
+        T f;
+        U s;
+        public Pair(T first, U sec){
+            this.f = first;
+            this.s = sec;
+        }
+    }
+    
+    static long bigPow(long base, long pow, long mod){
+        BigInteger res = new BigInteger("1");
+        BigInteger bBase = BigInteger.valueOf(base);
+        BigInteger mod2 = BigInteger.valueOf(mod);
+        while (pow > 0){
+            if(pow%2 == 1){
+                res = res.multiply(bBase);
+                res = res.mod(mod2);
+            }
+            bBase = bBase.pow(2);
+            bBase = bBase.mod(mod2);
+            pow >>= 1;
+        }
+        res = res.mod(BigInteger.valueOf(mod));
+        return res.longValue();
+    }
+
+    static long pow(long a, long b){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static long pow(long a, long b, long mod){
+        long res = 1;
+        while(b > 0){
+            if(b%2 == 1){
+                res = (res * a%mod)%mod;
+            }
+            a = (a%mod*a%mod)%mod;
+            b >>= 1;
+        }
+        return res%mod;
+    }
+
+    static boolean isPerfectSquare(long n){
+        long k = (long) Math.sqrt(n);
+        return (k*k) == n;
+    }
+
+    public static List<Integer> findFirstNPrimes(int n) {
+        List<Integer> primes = new ArrayList<>();
+        int num = 2;
+
+        while (primes.size() < n) {
+            if (isPrime(num)) {
+                primes.add(num);
+            }
+            num++;
+        }
+
+        return primes;
+    }
+
+    public static boolean isPrime(int n){
+        boolean flag = true;
+        for(int i = 2; i<=(int) Math.sqrt(n); i++){
+            if(n%i == 0){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    record  Triplet<T>(T x, T y, T z){
+
+    }
+
+    static Triplet<Long> extendedEuclid(long a, long b){ // a>b
+        if(b == 0){
+            return new Triplet<>(a, 0L, a);
+        }
+
+        Triplet<Long> smallAns = extendedEuclid(b, a%b);
+        long y = smallAns.x - (a/b)*smallAns.y;
+        return new Triplet<>(smallAns.y, y, smallAns.z);
+
+    }
+
+    static long modularMultiplicativeInverse(long a, long m){
+        long gcd = gcd(a, m);
+        if(gcd != 1){
+            return -1;
+        }
+        long x = extendedEuclid(a, m).x;
+        x = (x%m + m)%m;
+        return x;
+    }
+
+    static long modDivide(long a, long b, long m){
+        long inv = modularMultiplicativeInverse(b, m);
+        if(inv == -1){
+            return -1;
+        }
+        a %= m;
+        return (inv * a)%m;
+    }  
+    
+    static long [] maxSubArr(int [] arr){
+        long globalSum = Integer.MIN_VALUE;
+        long max = Integer.MIN_VALUE;
+        long n = arr.length;
+        int i1 = 0, i2 = 0, j1 = 0, j2 = 0;
+        for(int i = 0; i<n; i++){
+            if(arr[i] > globalSum+arr[i]){
+                globalSum = arr[i];
+                i2 = i;
+            } else{
+                globalSum += arr[i];
+            }
+            j2 = i;
+            if(max < globalSum){
+                max = globalSum;
+                i1 = i2; j1 = j2;
+            }
+        }
+        return new long[] {i1, j1, max};
+    }
+    
+    static boolean millerRobin(long n, long d, long a){
+        long x = bigPow(a, d, n);
+
+        if(x == 1 || x == n-1) return true;
+
+        while (d != n-1){
+            BigInteger bx = BigInteger.valueOf(x);
+            bx = bx.multiply(bx).mod(BigInteger.valueOf(n));
+            x = bx.longValue();
+            d *= 2;
+
+            if(x == 1) return false;
+            if(x == n-1) return true;
+        }
+
+        return false;
+    }
+    public static boolean isPrime(long n){
+        if(n <= 1 || n== 4) return false;
+        if(n <= 3) return true;
+        if(n%2 == 0) return false;
+
+        long d = n-1;
+        while ((d&1) == 0) d /= 2;
+
+        long [] arr = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+        for(long j : arr){
+            if(n == j) return true;
+            if(!millerRobin(n, d, j)) return false;
+        }
+
+        return true;
+    }
+
+
+    public static void main (String[] args) throws java.lang.Exception {
+        FastWriter out = new FastWriter();
+        int t = in.nextInt();
+        while(t-- > 0) {
+            long n = in.nextLong();
+            while (n > 1){
+                if(isPrime(n)) break;
+                n--;
+            }
+            System.out.println(n);
+        }
+        out.close();
+    }
+}
